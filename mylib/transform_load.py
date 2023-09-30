cb2511029d71e570b3c79688bb74d34ca04b0a88
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-09-30 17:59:25 -0400

    Update transform_load.py

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -1,19 +1,19 @@
 import sqlite3
 import csv
-#import os
 
-#load the csv file and insert into a new sqlite3 database
 def load(file_path="cars.csv"):
     """Transforms and Loads data into the local SQLite3 database"""
     
     with open(file_path, 'r', newline='') as f:
         csv_reader = csv.reader(f, delimiter=';')
-        next(csv_reader)
-        payload = list(csv_reader)
-        
+        next(csv_reader)  
+        payload = [row for row in csv_reader if row]  
+
+
     for row in payload:
         if len(row) != 9:
             print(f"Incorrect number of fields in row: {row}")
+
     conn = sqlite3.connect('CarsDB.db')
     c = conn.cursor()
     c.execute("DROP TABLE IF EXISTS CarsDB")
