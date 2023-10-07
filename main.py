daa403ce351ca872ddd10ee4bebe4f72a1694b25
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-07 19:51:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,51 @@
 ETL-Query script
 """
 import sys
+import argparse
 from mylib.extract import extract
 from mylib.transform_load import load
 from mylib.query import query
 
-def main():
+def process_cli_args():
+    """Manage command line inputs"""
+    cli_parser = argparse.ArgumentParser(description="Enhanced ETL-Query tool")
+    cli_parser.add_argument(
+        "task",
+        choices=["data_extraction", "data_loading", "data_query", "complete_etl"],
+        help="Select the task to execute"
+    )
+    return cli_parser.parse_args()
+
+def execute_task():
+    cli_args = process_cli_args()
+
     try:
-        # Extract
-        print("Extracting data...")
-        file_path = extract() 
-        print(f"Data extraction completed successfully. Saved to {file_path}\n")
+        if cli_args.task == "data_extraction":
+            print("Initiating data extraction...")
+            data_file = extract()
+            print(f"Extraction successful. Data saved at {data_file}\n")
 
-        # Transform and Load
-        print("Transforming and loading data...")                         
-        load(file_path)                
-        print("Data transformation and loading completed successfully.\n")
+        elif cli_args.task == "data_loading":
+            print("Initiating data transformation and loading...")
+            load("cars.csv")  # Assuming the file name is cars.csv
+            print("Transformation and loading completed.\n")
 
-        # Query
-        print("Querying data...")
-        query()
-        print("Data querying completed successfully.\n")
+        elif cli_args.task == "data_query":
+            print("Initiating data querying...")
+            query()
+            print("Querying process completed.\n")
 
-    except Exception as e:
-        print(f"An error occurred: {e}", file=sys.stderr)
+        elif cli_args.task == "complete_etl":
+            print("Starting the full ETL process...")
+            data_file = extract()
+            print(f"Extraction successful. Data saved at {data_file}\n")
+            load(data_file)
+            print("Transformation and loading completed.\n")
+            query()
+            print("Querying process completed.\n")
+
+    except Exception as err:
+        print(f"An issue occurred: {err}", file=sys.stderr)
 
 if __name__ == "__main__":
-    main()
+    execute_task()
