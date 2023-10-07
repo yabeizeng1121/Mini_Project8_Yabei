f49be78386b098be743bc942d57f6729370efe9d
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-07 19:31:25 -0400

    Update transform_load.py

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -1,39 +1,52 @@
-import sqlite3
-import csv
+import os
+import pandas as pd
+from databricks import sql
+from dotenv import load_dotenv
 
-def load(file_path="cars.csv"):
-    """Transforms and Loads data into the local SQLite3 database"""
+def load_data(dataset_path="cars.csv"):
+    """Load data into the Azure Databricks database"""
     
-    with open(file_path, 'r', newline='') as f:
-        csv_reader = csv.reader(f, delimiter=';')
-        next(csv_reader)  
-        payload = [row for row in csv_reader if row]  
+    # Read the CSV file into a pandas DataFrame
+    data_frame = pd.read_csv(dataset_path, delimiter=",", skiprows=1)
+    
+    # Load environment variables
+    load_dotenv()
+    db_server = os.getenv("DB_SERVER")
+    token = os.getenv("DB_TOKEN")
+    api_path = os.getenv("DB_API_PATH")
+    
+    # Connect to Azure Databricks
+    with sql.connect(
+        server_hostname=db_server,
+        http_path=api_path,
+        access_token=token,
+    ) as conn:
+        cursor = conn.cursor()
+        
+        # Check if the table exists
+        cursor.execute("SHOW TABLES FROM default LIKE 'CarsDB'")
+        result = cursor.fetchall()
+        
+        # If the table doesn't exist, create it
+        if not result:
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS CarsDB (
+                    id int,
+                    brand string,
+                    model string,
+                    year int,
+                    price float
+                )
+                """
+            )
+            
+            # Insert data into the table
+            for _, row in data_frame.iterrows():
+                values = (_,) + tuple(row)
+                cursor.execute(f"INSERT INTO CarsDB VALUES {values}")
+        
+        cursor.close()
 
+    return "Data loaded successfully"
 
-    for row in payload:
-        if len(row) != 9:
-            print(f"Incorrect number of fields in row: {row}")
-
-    conn = sqlite3.connect('CarsDB.db')
-    c = conn.cursor()
-    c.execute("DROP TABLE IF EXISTS CarsDB")
-    
-    # Create table query split for better readability
-    create_table_query = (
-        "CREATE TABLE CarsDB ("
-        "Brand TEXT, Price REAL, Body TEXT, Mileage INTEGER, "
-        "EngineV REAL, Engine_Type TEXT, Registration TEXT, "
-        "Year INTEGER, Model TEXT)"
-    )
-    c.execute(create_table_query)
-    
-    # Insert data query
-    insert_data_query = (
-        "INSERT INTO CarsDB (Brand, Price, Body, Mileage, EngineV, Engine_Type, "
-        "Registration, Year, Model) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
-    )
-    c.executemany(insert_data_query, payload)
-    
-    conn.commit()
-    conn.close()
-    return "CarsDB.db"
