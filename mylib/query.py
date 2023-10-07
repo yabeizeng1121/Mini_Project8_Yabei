8c0d80ae2747c3195a50cd90c2fa6fb4ba6ecd62
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-07 19:24:17 -0400

    Update query.py

--- mylib/query.py
+++ mylib/query.py
@@ -1,17 +1,36 @@
 """Query the database"""
+import os
+from databricks import sql
+from dotenv import load_dotenv
 
-import sqlite3
+# Global variable for the log file
+LOG_PATH = "query_log.md"
 
+def add_to_log(query_str, response="N/A"):
+    """Append query and its result to a markdown log file"""
+    with open(LOG_PATH, "a") as log_file:
+        log_file.write(f"```sql\n{query_str}\n```\n\n")
+        log_file.write(f"```Databricks response\n{response}\n```\n\n")
 
-def query():
-    """Query the database for the top 5 rows of the CarsDB table"""
-    conn = sqlite3.connect("CarsDB.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM CarsDB LIMIT 5")
-    print("Top 5 rows of the CarsDB table:")
-    print(cursor.fetchall())
-    conn.close()
-    return "Success"
+def execute_query(query_str):
+    """Execute a user-provided query"""
+
+    load_dotenv()
+    db_server = os.getenv("SERVER_HOSTNAME")
+    token = os.getenv("ACCESS_TOKEN")
+    api_path = os.getenv("HTTP_PATH")
+    
+    with sql.connect(
+        server_hostname=db_server,
+        http_path=api_path,
+        access_token=token,
+    ) as conn:
+        cursor = conn.cursor()
+        cursor.execute(query_str)
+        result = cursor.fetchall()
+    cursor.close()
+    
+    add_to_log(query_str, result)
 
 
 
