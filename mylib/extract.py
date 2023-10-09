55537560802bece5865dc7aac0fd70b1f56806d4
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-09 14:33:55 -0400

    Update extract.py

--- mylib/extract.py
+++ mylib/extract.py
@@ -1,3 +1,4 @@
+
 import os
 import requests
 import pandas as pd
@@ -19,7 +20,7 @@ def extract(
     with requests.get(url2) as r:
         with open(file_path2, "wb") as f:
             f.write(r.content)
-    df = pd.read_csv(file_path2, error_bad_lines=False)
+    df = pd.read_csv(file_path2, engine='python', error_bad_lines=False)
 
     df_subset = df.head(121)
 
