f7cfdec3d9dac363befb6f020ed42e63ac074e6e
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-09 14:00:46 -0400

    Update query.py

--- mylib/query.py
+++ mylib/query.py
@@ -1,36 +1,36 @@
-"""Query the database"""
 import os
 from databricks import sql
 from dotenv import load_dotenv
 
-# Global variable for the log file
-LOG_PATH = "query_log.md"
+# Define a global variable for the log file
+LOG_FILE = "query_log.md"
 
-def add_to_log(query_str, response="N/A"):
-    """Append query and its result to a markdown log file"""
-    with open(LOG_PATH, "a") as log_file:
-        log_file.write(f"```sql\n{query_str}\n```\n\n")
-        log_file.write(f"```Databricks response\n{response}\n```\n\n")
+def log_query(query, result="none"):
+    """adds to a query markdown file"""
+    with open(LOG_FILE, "a") as file:
+        file.write(f"```sql\n{query}\n```\n\n")
+        file.write(f"```response from databricks\n{result}\n```\n\n")
 
-def query(query_str):
-    """Execute a user-provided query"""
+def general_query(query):
+    """runs a query a user inputs"""
 
     load_dotenv()
-    db_server = os.getenv("SERVER_HOSTNAME")
-    token = os.getenv("ACCESS_TOKEN")
-    api_path = os.getenv("HTTP_PATH")
-    
+    server_h = os.getenv("SERVER_HOSTNAME")
+    access_token = os.getenv("ACCESS_TOKEN")
+    http_path = os.getenv("HTTP_PATH")
     with sql.connect(
-        server_hostname=db_server,
-        http_path=api_path,
-        access_token=token,
-    ) as conn:
-        cursor = conn.cursor()
-        cursor.execute(query_str)
-        result = cursor.fetchall()
-    cursor.close()
-    
-    add_to_log(query_str, result)
+        server_hostname=server_h,
+        http_path=http_path,
+        access_token=access_token,
+    ) as connection:
+        c = connection.cursor()
+        c.execute(query)
+        result = c.fetchall()
+    c.close()
+    log_query(f"{query}", result)
+
+# You can call the function like this:
+# general_query("YOUR SQL QUERY HERE")
 
 
 
