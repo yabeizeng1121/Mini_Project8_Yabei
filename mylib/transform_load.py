5d763659a2e0fac63cf49fd38d9f4defec575b98
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-09 15:09:00 -0400

    Update transform_load.py

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -15,32 +15,36 @@ def load(dataset="data/dem_candidates.csv", dataset2="data/rep_candidates.csv"):
     access_token = os.getenv("ACCESS_TOKEN")
     http_path = os.getenv("HTTP_PATH")
     
+
+
     with sql.connect(
-    server_hostname=server_h,
-    http_path=http_path,
-    access_token=access_token,
+        server_hostname=server_h,
+        http_path=http_path,
+        access_token=access_token,
     ) as connection:
-    c = connection.cursor()
-    
-    # Check if DemCandidatesDB table exists
-    c.execute("SHOW TABLES FROM default LIKE 'dem_candidates*'")
-    result = c.fetchall()
-    if not result:
-        c.execute(
-            """
-            CREATE TABLE IF NOT EXISTS DemCandidatesDB (
-                Name string,
-                Age int,
-                Occupation string,
-                State string,
-                Donations int
+        c = connection.cursor()
+        
+        # Check if DemCandidatesDB table exists
+        c.execute("SHOW TABLES FROM default LIKE 'dem_candidates*'")
+        result = c.fetchall()
+        if not result:
+            c.execute(
+                """
+                CREATE TABLE IF NOT EXISTS DemCandidatesDB (
+                    Name string,
+                    Age int,
+                    Occupation string,
+                    State string,
+                    Donations int
+                )
+                """
             )
-            """
-        )
-        # Insert data into DemCandidatesDB
-        for _, row in df.iterrows():
-            convert = tuple(row)
-            c.execute(f"INSERT INTO DemCandidatesDB VALUES {convert}")
+            # Insert data into DemCandidatesDB
+            for _, row in df.iterrows():
+                convert = tuple(row)
+                c.execute(f"INSERT INTO DemCandidatesDB VALUES {convert}")
+
+
 
         
         # Check if RepCandidatesDB table exists
