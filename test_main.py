ecef106fe1148a78f5aaa3657f137124af90bfc5
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-09 23:03:54 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -33,7 +33,8 @@ def test_general_query():
             "python",
             "main.py",
             "general_query",
-            """SELECT Performer, Show, MIN(Show_Start) AS Earliest_Show_Start, MAX(Show_End) AS Latest_Show_End
+            """SELECT Performer, Show, 
+            MIN(Show_Start) AS Earliest_Show_Start,MAX(Show_End) AS Latest_Show_End
             FROM showdataDB
             GROUP BY Performer, Show
             ORDER BY Earliest_Show_Start DESC
