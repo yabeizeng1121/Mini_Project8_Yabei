24a96878375d81be25bb022c9afdb769b5e5e63f
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-09 22:57:49 -0400

    Update transform_load.py

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -67,7 +67,7 @@ def load(dataset="data/performer-scores.csv", dataset2="data/show-data.csv"):
         # Insert data into showdataDB
         for row in df2[1:]:  # Skip the header row
             values = tuple(row)
-            if not values or len(values) != 5:  # Check if values is empty or does not match the expected length
+            if not values or len(values) != 5:  
                 print(f"Skipping invalid row: {row}")
                 continue
             try:
